bf243bc5ef1c46315bfcf7cf07a5894d83ac6e03
SABTECH <sabtech2011@gmail.com>
2025-02-09 14:52:47 +0300

    update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit65fdaf02cf2a4dc9241e927f36d36b8e::getLoader();
+return ComposerAutoloaderInitfe33f98a750b8c5a51b30c78bd3fab21::getLoader();
