d7ff94e885d99e51d80d889f9a9ba6ba9737cc7f
SABTECH <sabtech2011@gmail.com>
2025-02-09 13:12:58 +0300

    delete

--- app/Http/Kernel.php
+++
@@ -1,7 +0,0 @@
-<?php
-'bidding' => \App\Http\Middleware\BiddingAccess::class,
-protected $routeMiddleware = [
-    // ... other middlewares
-    'admin' => \App\Http\Middleware\AdminAccess::class,
-];
-}
